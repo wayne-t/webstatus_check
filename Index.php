dd458afebb6a620339a52dbdef932c290a45d9cc
unknown <Vobadm@M11-11633.corp.novellus.com>
2013-09-14 20:38:59 -0700

    webpage done

--- Index.php
+++ Index.php
@@ -1,4 +1,32 @@
-<html><body>
+<?php 
+
+$url = "";
+session_start();
+
+if(isset($_POST['submit']) && (($_POST['link']) != ""))
+{
+  $_SESSION['text'] = $_POST['link'];
+  header("Location: ". $_SERVER['REQUEST_URI']);
+  exit;
+}
+else
+{
+  if(isset($_SESSION['text']))
+  {
+    $url = $_SESSION['text'];
+    unset($_SESSION['text']);
+  }
+}
+?>
+
+<!DOCTYPE html>
+
+<head>
+<title>Check Web Status</title>
+</head>
+<body>
+<br/><h2> Hello </h2>
+
 <?php
 function Visit($url){
        $agent = "Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0)";$ch=curl_init();
@@ -17,9 +45,30 @@ function Visit($url){
        if($httpcode>=200 && $httpcode<400) return true;
        else return false;
 }
-if (Visit("http://www.google.com"))
-       echo "Website OK"."n";
+function Print_Form()
+{
+echo "<h3> Enter a webpage and see whether it's up </h3></p>";
+echo "<form  method=\"post\">";
+echo "<p>URL: <input type=\"text\" name=\"link\" /></p>";
+echo "<p><input type=\"submit\" name=\"submit\" value=\"check\" />";
+echo "</form>";
+}
+
+if($url != "")
+{
+  if (Visit($url))
+  {  Print_Form();
+       echo "<br> <h3> Website '$url' is OK </h3>";
+  }
+  else
+  {  Print_Form();
+       echo "<br> <h3> Website '$url' is DOWN </h3>";
+  }
+}
 else
-       echo "Website DOWN";
+{
+  Print_Form();
+} 
 ?>
-</html></body>
+</body>
+</html>
