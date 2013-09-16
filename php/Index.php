687672fac20ed35af52a50e81559e566684bc32b
web-deploy <waynetsui@hotmail.com>
2013-09-16 02:32:50 -0700

    Update Index.php

--- php/Index.php
+++ php/Index.php
@@ -29,21 +29,21 @@ else
 
 <?php
 function Visit($url){
-       $agent = "Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0)";$ch=curl_init();
-       curl_setopt ($ch, CURLOPT_URL,$url );
-       curl_setopt($ch, CURLOPT_USERAGENT, $agent);
-       curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
-       curl_setopt ($ch,CURLOPT_VERBOSE,false);
-       curl_setopt($ch, CURLOPT_TIMEOUT, 5);
-       curl_setopt($ch,CURLOPT_SSL_VERIFYPEER, FALSE);
-       curl_setopt($ch,CURLOPT_SSLVERSION,3);
-       curl_setopt($ch,CURLOPT_SSL_VERIFYHOST, FALSE);
-       $page=curl_exec($ch);
-       //echo curl_error($ch);
-       $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-       curl_close($ch);
-       if($httpcode>=200 && $httpcode<400) return true;
-       else return false;
+  $agent = "Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0)";$ch=curl_init();
+  curl_setopt ($ch, CURLOPT_URL,$url );
+  curl_setopt($ch, CURLOPT_USERAGENT, $agent);
+  curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt ($ch,CURLOPT_VERBOSE,false);
+  curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+  curl_setopt($ch,CURLOPT_SSL_VERIFYPEER, FALSE);
+  curl_setopt($ch,CURLOPT_SSLVERSION,3);
+  curl_setopt($ch,CURLOPT_SSL_VERIFYHOST, FALSE);
+  $page=curl_exec($ch);
+  //echo curl_error($ch);
+  $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+  curl_close($ch);
+  if($httpcode>=200 && $httpcode<400) return true;
+  else return false;
 }
 function Print_Form()
 {
@@ -58,7 +58,7 @@ if($url != "")
 {
   if (Visit($url))
   {  Print_Form();
-       echo "<br> <h3> Website '$url' is OK </h3>";
+       echo "<br> <h3> Website '$url' is UP </h3>";
   }
   else
   {  Print_Form();
